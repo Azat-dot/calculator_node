88189dcb233a17beadb1d6ed3a9ec93d081db711
azat <azatsafaryan@mail.ru>
2022-06-25 12:29:13 +0500

    first commit

---
+++ calculator.js
@@ -0,0 +1,14 @@
+//jshint esversion:6
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", function(req, res){
+    res.send("<h1>Hello, World! </h1>")
+})
+
+
+app.listen(3000, function(){
+    console.log("Server startedon port 3000");
+})
