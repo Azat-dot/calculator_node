2edfea8e136fd454d9e0f56f4e1e116f88eac135
azat <azatsafaryan@mail.ru>
2022-06-27 12:06:10 +0500

    working calculator

--- calculator.js
+++ calculator.js
@@ -1,14 +1,24 @@
 //jshint esversion:6
 
 const express = require("express");
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
-    res.send("<h1>Hello, World! </h1>")
+    res.sendFile(__dirname + "/index.html");
+});
+
+
+app.post('/', function(req, res){
+    
+    let num1 = Number(req.body.num1);
+    let num2 = Number(req.body.num2);
+    let result = num1 + num2;
+
+    res.send("Итог будет равен" + result)
 })
-
-
 app.listen(3000, function(){
     console.log("Server startedon port 3000");
 })
